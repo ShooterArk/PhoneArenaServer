b06a164390626d5b952ddf8a74256148c9bc4cb0
ShooterArk <smallassasin@gmail.com>
2021-07-23 17:24:14 +0500

    Refactored the code.

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,13 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
+var phones = require('../data/phones.json')
 
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const data = await readFile();
-  console.log(data);
-  res.status(200).send({status:200,model:  data});
+  res.status(200).send({status: true, model: phones});
 });
 
-const readFile = () => {
-  return new Promise((resolve,reject) => {
-    try{
-      var processedFile = path.resolve(__dirname + "/phones.json");
-  
-  fs.readFile(processedFile, 'utf8', async function(err, data) {
-    if (err) {
-      console.log(err);
-      reject(err);
-    }
-    obj = JSON.parse(data);
-    resolve(obj);
-    //res.send(s3Result);
-  });
-    }catch(error){
-      reject(error);
-    }
-  })
-  
-}
 
 module.exports = router;
